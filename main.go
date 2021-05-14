bb25e6cc976f779cd5c1847e681f0de9f2216b03
Cosiek <fitek2@o2.pl>
2021-05-14 14:22:29 +0200

    Hello World - pantry package

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "cooking/m/v2/pantry"
+
+func main() {
+	pantry.StartServer()
+}
---
+++ pantry/server.go
@@ -0,0 +1,19 @@
+package pantry
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world!") // send data to client side
+}
+
+func StartServer() {
+	http.HandleFunc("/", helloWorld)         // set router
+	err := http.ListenAndServe(":8080", nil) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
